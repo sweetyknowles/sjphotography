c72a3775208349c059c80100d7624f9028f0eda4
SweetyKnowles <sweetyjames@hotmail.co.uk>
2018-08-02 15:36:23 -0400

    completed basic layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
-import NavBar from './components/NavBar'
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from './components/Home';
+import Photo from './components/Photo';
+import Contact from './components/Contact';
 
 class App extends Component {
-  render () {
+  render() {
     return (
-      <Router>
+      <HashRouter>
         <div>
-          <NavBar />
-          <Switch>
-            {/* <Route exact path="/" component={ArtistListView} /> */}
+          <h1>Simple SPA</h1>
+          <ul className="header">
+          <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/photo">Photo</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+          <Route path="/" component={Home}/>
+            <Route path="/photo" component={Photo}/>
+            <Route path="/contact" component={Contact}/>
              
-          </Switch>
+          </div>
         </div>
-      </Router>
-    )
+        </HashRouter>
+    );
   }
 }
-
-export default App
+ 
+export default App;
