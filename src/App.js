917c291c30c2a4ac660a0422d2f09c444c77774a
SweetyKnowles <sweetyjames@hotmail.co.uk>
2018-08-25 20:47:40 -0400

    loading images

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
+
 import React, { Component } from "react";
-import {
-  Route,
-  NavLink,
-  HashRouter
-} from "react-router-dom";
+import {Route,NavLink,HashRouter, } from "react-router-dom";
+
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Photo from './components/Photo';
+
 
 class App extends Component {
   render() {
     return (
       <HashRouter>
         <div>
-          <h1>Simple SPA</h1>
+          <h1>Photography is a PasSion</h1>
           <ul className="header">
           <li><NavLink exact to="/">Home</NavLink></li>
             <li><NavLink to="/about">About</NavLink></li>
             <li><NavLink to="/contact">Contact</NavLink></li>
+            <li><NavLink to="/photo">Photos</NavLink></li>
+            
           </ul>
           <div className="content">
           <Route path="/" component={Home}/>
             <Route path="/about" component={About}/>
             <Route path="/contact" component={Contact}/>
-             
+            <Route path="/photo" component={Photo}/>
+            
           </div>
         </div>
         </HashRouter>
---
+++ src/components/Photo.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import ImageGallery from 'react-image-gallery'
+
+
+
+class Photo extends Component {
+    render() {
+
+        const photos = [{
+            //photo1:'https://i.kinja-img.com/gawker-media/image/upload/s--YFybMImn--/c_scale,f_auto,fl_progressive,q_80,w_800/w8xvxzrj8kn1iuzrjnqt.jpg',
+
+            original: 'http://lorempixel.com/1000/600/nature/1/',
+            thumbnail: 'http://lorempixel.com/250/150/nature/1/',
+          },
+          {
+            original: 'http://lorempixel.com/1000/600/nature/2/',
+            thumbnail: 'http://lorempixel.com/250/150/nature/2/'
+          },
+          {
+            original: 'http://lorempixel.com/1000/600/nature/3/',
+            thumbnail: 'http://lorempixel.com/250/150/nature/3/'
+          }
+
+
+        ]
+
+
+        return (
+            <ImageGallery items={photos} />
+            
+        );
+    }
+}
+
+export default Photo;
